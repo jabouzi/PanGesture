c87ceacf816f4c07138501ca034c670552f6c690
Apoorv Mote <apoorvmote@apoorvs-macbook-air.local>
2015-10-04 00:38:40 +0530

    Initial Commit

---
+++ pan gesture demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pan gesture demo
+//
+//  Created by Apoorv Mote on 04/10/15.
+//  Copyright © 2015 Apoorv Mote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
